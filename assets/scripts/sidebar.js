dde741556e1a5e1636b690fd7a11504574ee1fc8
MestoloPomello <stefanonava99@gmail.com>
2024-07-01 02:05:30 +0200

    sidebar experiments

--- assets/scripts/sidebar.js
+++ assets/scripts/sidebar.js
@@ -1,15 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuItems = document.querySelectorAll('.menu-item.has-submenu');
+document.addEventListener("DOMContentLoaded", function () {
 
+    // Menu toggle button
+    const sidebar = document.querySelector('.sidebar');
+    const header = document.querySelector('.header');
+
+    header.addEventListener('click', function () {
+        sidebar.classList.toggle('collapsed');
+
+        // Sidebar positioning
+        if (header) {
+            const headerRect = header.getBoundingClientRect();
+            sidebar.style.top = `${headerRect.bottom}px`;
+        }        
+    });
+
+    // Submenu handler
+    const menuItems = document.querySelectorAll('.menu-item.has-submenu');
     menuItems.forEach(item => {
-        item.addEventListener('mouseenter', function() {
+        item.addEventListener('mouseenter', function () {
             const submenu = item.querySelector('.submenu');
             const itemRect = item.getBoundingClientRect();
             submenu.style.top = `${itemRect.top}px`;
             submenu.style.display = 'block';
         });
 
-        item.addEventListener('mouseleave', function() {
+        item.addEventListener('mouseleave', function () {
             const submenu = item.querySelector('.submenu');
             submenu.style.display = 'none';
         });
